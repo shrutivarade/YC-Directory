7e13e1a61548d6c99a2f5335264b5c6bab4469bb
Shruti Varade <shrutivarade27@gmail.com>
2025-01-07 21:26:05 -0500

    Shadcn setup done to reuse commonly used components, Hero section part 1 ready.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className='heading'>HOME</h1>
+        <section className="pink_container">
+            <h1 className="heading">
+                Pitch Your Startup, <br/>
+                Connect With Entrepreneurs
+            </h1>
+
+            <p className="sub-heading !max-w-3xl">
+                Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+                Competitions.
+            </p>
+        </section>
     </>
   );
 }
